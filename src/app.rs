b99fbf37e5c3e1aa8f1b3a00d5d79193f6e246f5
rayan <rayan13170@protonmail.com>
2024-05-23 00:47:47 +0200

    made a clean structure

--- src/app.rs
+++ src/app.rs
@@ -19,6 +19,23 @@ Examples:
     You can also omit multiple folders
 
     $ fnr old . --omit target/ build/
+
+    Including hidden files
+
+    $ fnr old . --hidden --omit target/ build/
+
+    Only search for files with a specific extension
+
+    $ fnr old . --type .rs
+
+    Ignore files with a specific extension
+
+    $ fnr old . --type-not .rs
+
+    You can also search / ignore multiple file types
+    Here, we search for files with .rs and .toml extension, but ignore .txt and .md files
+
+    $ fnr old . --type .rs .toml --type-not .txt .md
 "
 )]
 pub struct Options {
@@ -26,6 +43,7 @@ pub struct Options {
     pub pattern: String,
 
     /// The path of the folder / file to read
+    /// Default is the current directory
     pub path: Option<PathBuf>,
 
     #[arg(long, help = "Include hidden files in the search")]
@@ -63,13 +81,56 @@ pub struct Options {
     ignored_file_types: Vec<String>,
 }
 
+
+
+
+pub fn run() {
+    let args = Options::parse();
+    println!("{:?}", args);
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
 
     // use super::*;
+    use assert_cmd::Command;
 
     #[test]
-    fn test_args_are_parsed() {
-        
+    fn test_no_arguments() {
+        let mut cmd = Command::cargo_bin("fnr").unwrap();
+        cmd.assert().failure();
+    }
+
+    #[test]
+    fn test_basic_search() {
+        let mut cmd = Command::cargo_bin("fnr").unwrap();
+        cmd.arg("old").assert().success();
+    }
+
+    #[test]
+    fn test_omit() {
+        let mut cmd = Command::cargo_bin("fnr").unwrap();
+        cmd.args(&["old", ".", "--omit", "target"])
+            .assert()
+            .success();
+    }
+
+    #[test]
+    fn test_hidden_verbose() {
+        let mut cmd = Command::cargo_bin("fnr").unwrap();
+        cmd.args(&["old", ".", "--hidden", "--verbose"])
+            .assert()
+            .success();
+    }
+
+    #[test]
+    fn test_file_types() {
+        let mut cmd = Command::cargo_bin("fnr").unwrap();
+        cmd.args(&["old", ".", "-t", "rs", "-T", "json"])
+            .assert()
+            .success();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use fnr::app::Options;
-use clap::Parser;
 
 fn main() {
-    let opts: Options = Options::parse();
-    
-    println!("{:?}", opts);
+    fnr::run();
 }
